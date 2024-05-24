223bb8f6273ad85f1821552282626e95770fde6a
1DragonScripter1 <fhuojxp38sk@gmail.com>
2024-05-24 13:27:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Lab_24_Shovkoplias
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Кнопки керування
+        private System.Windows.Forms.Button btnStartRedoc;
+        private System.Windows.Forms.Button btnStopRedoc;
+        private System.Windows.Forms.Button btnStartMdc2;
+        private System.Windows.Forms.Button btnStopMdc2;
+        private System.Windows.Forms.Button btnStartPkzip;
+        private System.Windows.Forms.Button btnStopPkzip;
+        private System.Windows.Forms.Button btnStartAll;
+        private System.Windows.Forms.Button btnStopAll;
+
+        // Поля вивода логов для алгоритмів
+        private System.Windows.Forms.TextBox textBoxRedoc;
+        private System.Windows.Forms.TextBox textBoxMdc2;
+        private System.Windows.Forms.TextBox textBoxPkzip;
+
+        /// <summary>
+        /// Освобождает все используемые ресурсы
+        /// </summary>
+        /// <param name="disposing">true, если управляемый ресурс должен быть удален; иначе false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически сгенерированный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartRedoc = new System.Windows.Forms.Button();
+            this.btnStopRedoc = new System.Windows.Forms.Button();
+            this.btnStartMdc2 = new System.Windows.Forms.Button();
+            this.btnStopMdc2 = new System.Windows.Forms.Button();
+            this.btnStartPkzip = new System.Windows.Forms.Button();
+            this.btnStopPkzip = new System.Windows.Forms.Button();
+            this.btnStartAll = new System.Windows.Forms.Button();
+            this.btnStopAll = new System.Windows.Forms.Button();
+            this.textBoxRedoc = new System.Windows.Forms.TextBox();
+            this.textBoxMdc2 = new System.Windows.Forms.TextBox();
+            this.textBoxPkzip = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStartRedoc
+            // 
+            this.btnStartRedoc.Location = new System.Drawing.Point(20, 270);
+            this.btnStartRedoc.Name = "btnStartRedoc";
+            this.btnStartRedoc.Size = new System.Drawing.Size(100, 30);
+            this.btnStartRedoc.TabIndex = 0;
+            this.btnStartRedoc.Text = "Start REDOC";
+            this.btnStartRedoc.UseVisualStyleBackColor = true;
+            // 
+            // btnStopRedoc
+            // 
+            this.btnStopRedoc.ForeColor = System.Drawing.Color.Red;
+            this.btnStopRedoc.Location = new System.Drawing.Point(20, 310);
+            this.btnStopRedoc.Name = "btnStopRedoc";
+            this.btnStopRedoc.Size = new System.Drawing.Size(100, 30);
+            this.btnStopRedoc.TabIndex = 1;
+            this.btnStopRedoc.Text = "Stop REDOC";
+            this.btnStopRedoc.UseVisualStyleBackColor = true;
+            // 
+            // btnStartMdc2
+            // 
+            this.btnStartMdc2.Location = new System.Drawing.Point(160, 270);
+            this.btnStartMdc2.Name = "btnStartMdc2";
+            this.btnStartMdc2.Size = new System.Drawing.Size(100, 30);
+            this.btnStartMdc2.TabIndex = 2;
+            this.btnStartMdc2.Text = "Start MDC-2";
+            this.btnStartMdc2.UseVisualStyleBackColor = true;
+            // 
+            // btnStopMdc2
+            // 
+            this.btnStopMdc2.ForeColor = System.Drawing.Color.Red;
+            this.btnStopMdc2.Location = new System.Drawing.Point(160, 310);
+            this.btnStopMdc2.Name = "btnStopMdc2";
+            this.btnStopMdc2.Size = new System.Drawing.Size(100, 30);
+            this.btnStopMdc2.TabIndex = 3;
+            this.btnStopMdc2.Text = "Stop MDC-2";
+            this.btnStopMdc2.UseVisualStyleBackColor = true;
+            // 
+            // btnStartPkzip
+            // 
+            this.btnStartPkzip.Location = new System.Drawing.Point(300, 270);
+            this.btnStartPkzip.Name = "btnStartPkzip";
+            this.btnStartPkzip.Size = new System.Drawing.Size(100, 30);
+            this.btnStartPkzip.TabIndex = 4;
+            this.btnStartPkzip.Text = "Start PKZIP";
+            this.btnStartPkzip.UseVisualStyleBackColor = true;
+            // 
+            // btnStopPkzip
+            // 
+            this.btnStopPkzip.ForeColor = System.Drawing.Color.Red;
+            this.btnStopPkzip.Location = new System.Drawing.Point(300, 310);
+            this.btnStopPkzip.Name = "btnStopPkzip";
+            this.btnStopPkzip.Size = new System.Drawing.Size(100, 30);
+            this.btnStopPkzip.TabIndex = 5;
+            this.btnStopPkzip.Text = "Stop PKZIP";
+            this.btnStopPkzip.UseVisualStyleBackColor = true;
+            // 
+            // btnStartAll
+            // 
+            this.btnStartAll.Location = new System.Drawing.Point(420, 270);
+            this.btnStartAll.Name = "btnStartAll";
+            this.btnStartAll.Size = new System.Drawing.Size(100, 30);
+            this.btnStartAll.TabIndex = 6;
+            this.btnStartAll.Text = "Start All";
+            this.btnStartAll.UseVisualStyleBackColor = true;
+            // 
+            // btnStopAll
+            // 
+            this.btnStopAll.ForeColor = System.Drawing.Color.Red;
+            this.btnStopAll.Location = new System.Drawing.Point(420, 310);
+            this.btnStopAll.Name = "btnStopAll";
+            this.btnStopAll.Size = new System.Drawing.Size(100, 30);
+            this.btnStopAll.TabIndex = 7;
+            this.btnStopAll.Text = "Stop All";
+            this.btnStopAll.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRedoc
+            // 
+            this.textBoxRedoc.Location = new System.Drawing.Point(20, 20);
+            this.textBoxRedoc.Multiline = true;
+            this.textBoxRedoc.Name = "textBoxRedoc";
+            this.textBoxRedoc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxRedoc.Size = new System.Drawing.Size(200, 230);
+            this.textBoxRedoc.TabIndex = 8;
+            // 
+            // textBoxMdc2
+            // 
+            this.textBoxMdc2.Location = new System.Drawing.Point(240, 20);
+            this.textBoxMdc2.Multiline = true;
+            this.textBoxMdc2.Name = "textBoxMdc2";
+            this.textBoxMdc2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMdc2.Size = new System.Drawing.Size(200, 230);
+            this.textBoxMdc2.TabIndex = 9;
+            // 
+            // textBoxPkzip
+            // 
+            this.textBoxPkzip.Location = new System.Drawing.Point(460, 20);
+            this.textBoxPkzip.Multiline = true;
+            this.textBoxPkzip.Name = "textBoxPkzip";
+            this.textBoxPkzip.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxPkzip.Size = new System.Drawing.Size(200, 230);
+            this.textBoxPkzip.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 360);
+            this.Controls.Add(this.textBoxPkzip);
+            this.Controls.Add(this.textBoxMdc2);
+            this.Controls.Add(this.textBoxRedoc);
+            this.Controls.Add(this.btnStopAll);
+            this.Controls.Add(this.btnStartAll);
+            this.Controls.Add(this.btnStopPkzip);
+            this.Controls.Add(this.btnStartPkzip);
+            this.Controls.Add(this.btnStopMdc2);
+            this.Controls.Add(this.btnStartMdc2);
+            this.Controls.Add(this.btnStopRedoc);
+            this.Controls.Add(this.btnStartRedoc);
+            this.Name = "Form1";
+            this.Text = "Encryption Algorithms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_24_Shovkoplias
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource ctsRedoc, ctsMdc2, ctsPkzip;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Обробник подій кнопок
+            btnStartRedoc.Click += (s, ev) => StartRedoc(textBoxRedoc);
+            btnStopRedoc.Click += (s, ev) => StopRedoc();
+            btnStartMdc2.Click += (s, ev) => StartMdc2(textBoxMdc2);
+            btnStopMdc2.Click += (s, ev) => StopMdc2();
+            btnStartPkzip.Click += (s, ev) => StartPkzip(textBoxPkzip);
+            btnStopPkzip.Click += (s, ev) => StopPkzip();
+            btnStartAll.Click += (s, ev) => { StartRedoc(textBoxRedoc); StartMdc2(textBoxMdc2); StartPkzip(textBoxPkzip); };
+            btnStopAll.Click += (s, ev) => { StopRedoc(); StopMdc2(); StopPkzip(); };
+        }
+
+        private void StartRedoc(TextBox tb)
+        {
+            if (ctsRedoc == null || ctsRedoc.IsCancellationRequested)
+            {
+                ctsRedoc = new CancellationTokenSource();
+                Task.Run(() => PerformRedocEncryption(ctsRedoc.Token, tb));
+            }
+        }
+
+        private void StopRedoc()
+        {
+            if (ctsRedoc != null && !ctsRedoc.IsCancellationRequested)
+            {
+                ctsRedoc.Cancel();
+            }
+        }
+
+        private void StartMdc2(TextBox tb)
+        {
+            if (ctsMdc2 == null || ctsMdc2.IsCancellationRequested)
+            {
+                ctsMdc2 = new CancellationTokenSource();
+                Task.Run(() => PerformMdc2Hashing(ctsMdc2.Token, tb));
+            }
+        }
+
+        private void StopMdc2()
+        {
+            if (ctsMdc2 != null && !ctsMdc2.IsCancellationRequested)
+            {
+                ctsMdc2.Cancel();
+            }
+        }
+
+        private void StartPkzip(TextBox tb)
+        {
+            if (ctsPkzip == null || ctsPkzip.IsCancellationRequested)
+            {
+                ctsPkzip = new CancellationTokenSource();
+                Task.Run(() => PerformPkzipEncryption(ctsPkzip.Token, tb));
+            }
+        }
+
+        private void StopPkzip()
+        {
+            if (ctsPkzip != null && !ctsPkzip.IsCancellationRequested)
+            {
+                ctsPkzip.Cancel();
+            }
+        }
+
+        private async Task PerformRedocEncryption(CancellationToken token, TextBox tb)
+        {
+            var rand = new Random();
+            tb.Invoke(() => tb.Clear());
+
+            while (!token.IsCancellationRequested)
+            {
+                string plaintext = GenerateRandomString(32, rand);
+                string encrypted = EncryptREDOC(plaintext);
+
+                tb.Invoke(() => tb.AppendText($"Original: {plaintext}\nEncrypted (REDOC): {encrypted}\n\n"));
+
+                await Task.Delay(1000, token).ConfigureAwait(false);
+            }
+        }
+
+        private async Task PerformMdc2Hashing(CancellationToken token, TextBox tb)
+        {
+            var rand = new Random();
+            tb.Invoke(() => tb.Clear());
+
+            while (!token.IsCancellationRequested)
+            {
+                string plaintext = GenerateRandomString(32, rand);
+                string hash = ComputeMDC2(plaintext);
+
+                tb.Invoke(() => tb.AppendText($"Original: {plaintext}\nMDC-2 Hash: {hash}\n\n"));
+
+                await Task.Delay(1000, token).ConfigureAwait(false);
+            }
+        }
+
+        private async Task PerformPkzipEncryption(CancellationToken token, TextBox tb)
+        {
+            var rand = new Random();
+            tb.Invoke(() => tb.Clear());
+
+            while (!token.IsCancellationRequested)
+            {
+                string plaintext = GenerateRandomString(32, rand);
+                string encrypted = EncryptPKZIP(plaintext);
+
+                tb.Invoke(() => tb.AppendText($"Original: {plaintext}\nEncrypted (PKZIP): {encrypted}\n\n"));
+
+                await Task.Delay(1000, token).ConfigureAwait(false);
+            }
+        }
+
+        private string EncryptREDOC(string plaintext)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(plaintext);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+
+        private string ComputeMDC2(string plaintext)
+        {
+            // Використовуємо MD5 замість MDC2, оскільки MDC-2 не підтримується безпосередньо
+            using (var md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(plaintext);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+
+        private string EncryptPKZIP(string plaintext)
+        {
+            using (var crc = new Crc32())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(plaintext);
+                byte[] hashBytes = crc.ComputeHash(inputBytes);
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+
+        private string GenerateRandomString(int length, Random rand)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(chars[rand.Next(chars.Length)]);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    // CRC32 Implementation
+    public class Crc32 : HashAlgorithm
+    {
+        public const uint DefaultPolynomial = 0xedb88320;
+        public const uint DefaultSeed = 0xffffffff;
+        private static uint[] defaultTable;
+        private readonly uint seed;
+        private readonly uint[] table;
+        private uint hash;
+
+        public Crc32()
+            : this(DefaultPolynomial, DefaultSeed)
+        {
+        }
+
+        public Crc32(uint polynomial, uint seed)
+        {
+            if (defaultTable == null)
+            {
+                defaultTable = InitializeTable(polynomial);
+            }
+
+            this.table = defaultTable;
+            this.seed = seed;
+            Initialize();
+        }
+
+        public static uint ComputeChecksum(byte[] bytes)
+        {
+            return ComputeChecksum(DefaultPolynomial, DefaultSeed, bytes);
+        }
+
+        public static uint ComputeChecksum(uint polynomial, uint seed, byte[] bytes)
+        {
+            var table = InitializeTable(polynomial);
+            uint crc = seed;
+            foreach (var t in bytes)
+            {
+                byte b = t;
+                unchecked
+                {
+                    crc = (crc >> 8) ^ table[(crc & 0xff) ^ b];
+                }
+            }
+            return ~crc;
+        }
+
+        public static byte[] ComputeChecksumBytes(byte[] bytes)
+        {
+            return BitConverter.GetBytes(ComputeChecksum(bytes));
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            hash = ComputeHashCore(table, hash, array, ibStart, cbSize);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            var hashBuffer = BitConverter.GetBytes(~hash);
+            Array.Reverse(hashBuffer);
+            return hashBuffer;
+        }
+
+        private static uint[] InitializeTable(uint polynomial)
+        {
+            var createTable = new uint[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                uint entry = (uint)i;
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((entry & 1) == 1)
+                    {
+                        entry = (entry >> 1) ^ polynomial;
+                    }
+                    else
+                    {
+                        entry >>= 1;
+                    }
+                }
+                createTable[i] = entry;
+            }
+
+            return createTable;
+        }
+
+        private static uint ComputeHashCore(uint[] table, uint seed, byte[] buffer, int start, int size)
+        {
+            uint crc = seed;
+            for (int i = start; i < start + size; i++)
+            {
+                unchecked
+                {
+                    crc = (crc >> 8) ^ table[(crc & 0xff) ^ buffer[i]];
+                }
+            }
+            return crc;
+        }
+    }
+
+    static class Extensions
+    {
+        public static void Invoke(this Control control, Action action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new MethodInvoker(action));
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
